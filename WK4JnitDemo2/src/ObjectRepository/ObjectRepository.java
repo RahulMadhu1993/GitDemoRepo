db8e118695b2e58cd4748c7e5d5679d41e60606c
admin <admin@admin-PC>
2019-06-15 07:47:29 +0530

    Initial Commit and Push

---
+++ WK4JnitDemo2/src/ObjectRepository/ObjectRepository.java
@@ -0,0 +1,13 @@
+package ObjectRepository;
+
+public class ObjectRepository {
+	
+	//Login
+	public static String txtUserName="userName";
+	public static String txtPassword="//input[@name='password']";
+	public static String btnLogin="login";
+	
+	//Logged in page
+	public static String lnkSignOff="SIGN-OFF";
+
+}
